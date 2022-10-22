72b210cab8dff81f374c08fed8c2ee35523f6e10
Krzysztof Frydryk <frydryk.krzysztof@gmail.com>
2022-10-23 01:03:23 +0200

    STM32F407G: init: Init platform
    Init main clock at 168MHz, use external xtal, configure flash.
    
    Signed-off-by: Krzysztof Frydryk <frydryk.krzysztof@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "test-driver.h"
 #include "test-lib.h"
+#include "platform/stm32f407G/include/platform_init.h"
 
 int main()
-{	
+{
+	platform_init();
+
 	int result = 0;
 
 	result = testlib(result);
---
+++ src/platform/stm32f407G/include/platform_init.h
@@ -0,0 +1,6 @@
+#ifndef PLATFORM_INIT_H
+#define PLATFORM_INIT_H
+
+void platform_init(void);
+
+#endif
