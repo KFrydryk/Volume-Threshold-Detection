0583788f400868765dffba17a48629bc169c3a6c
Krzysztof Frydryk <frydryk.krzysztof@gmail.com>
2022-10-23 07:17:09 +0200

    main: Interact with button and LED
    Add simple usage for GPIO driver
    
    Signed-off-by: Krzysztof Frydryk <frydryk.krzysztof@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,17 +1,30 @@
-#include "test-driver.h"
-#include "test-lib.h"
+#include <stddef.h>
 #include "platform/stm32f407G/include/platform_init.h"
+#include "gpio.h"
+
+#define BUTT_PORT 0
+#define BUTT_PIN 0
 
 int main()
 {
+	int pressed = 0;
+
 	platform_init();
+	gpio_periph_init(NULL);
 
-	int result = 0;
+	struct gpio BUT = gpio_create(BUTT_PORT, BUTT_PIN);
+	struct gpio LED = gpio_create(3, 13);
 
-	result = testlib(result);
-	result = testdriver(result);
+	BUT.ops->configure(&BUT, INPUT, NOT_OUT, PULLDOWN);
+	LED.ops->configure(&LED, OUTPUT, PUSH_PULL, NOT_IN);
 
-	while(1){}
+	while (1) {
+		pressed = BUT.ops->read_val(&BUT);
+		if (pressed)
+			LED.ops->write_val(&LED, HIGH);
+		else
+			LED.ops->write_val(&LED, LOW);
+	}
 	
 	return 0;
 }
